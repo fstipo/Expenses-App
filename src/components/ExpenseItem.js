8b648ad74f001b69da3b3dc369cbf9822a0bed8d
fstipo <fstipanov@gmail.com>
2022-04-12 09:25:30 +0200

    add ExpenseItem component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ExpenseItem.css';
+
+const ExpenseItem = () => {
+  const expenseDate = new Date('March 28, 2022');
+  const expenseTitle = 'Car Insurance';
+  const expensePrice = 294.67;
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expensePrice}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
