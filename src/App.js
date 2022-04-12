85e21cadba0d848d76b9a61cb07489c23dfb0a6b
fstipo <fstipanov@gmail.com>
2022-04-12 16:17:40 +0200

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Card from './Card';
-import ExpenseItem from './components/ExpenseItem';
+import Card from './components/UI/Card';
+import ExpenseItem from './components/Expenses/ExpenseItem';
 
 function App() {
   const expenses = [
