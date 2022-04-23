ce45db42359e8d927b0e51854390bbc21bf70479
fstipo <fstipanov@gmail.com>
2022-04-23 11:56:54 +0200

    clear input fields

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,7 +5,6 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
-  console.log('We are started!');
 
   const clickHandler = () => {
     console.log('Click!!!!');
